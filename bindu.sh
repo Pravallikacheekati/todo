13fbafa621d70a66c374b0555e185dbf7f8c5693
Pravallikacheekati <cheekatipravallika1505@gmail.com>
2024-01-12 18:17:23 +0530

    message

---
+++ bindu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+bindu="good girl"
+prava="lucky girl"
+
+bindu=$1
+prava=$2
+
+echo good girl
+echo lucky girl
---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+message="this is my first commit through shellscript"
+branch=master
+
+git init
+git add .
+git commit -m "message"
+git push origin master
+
+
